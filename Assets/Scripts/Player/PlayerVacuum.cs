1cd1682b59102e7e57649349939e8ae8cfb490af
teemu-siipola <Siipolateemu@gmail.com>
2022-01-27 13:22:03 +0200

    Player vacuuming.
    Mainly player vacuum behaviour and vfx.

---
+++ Assets/Scripts/Player/PlayerVacuum.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVacuum : MonoBehaviour
+{
+    public ParticleSystem _vfx;
+    public float vacuumRadius;
+    public float vacuumingAngle;
+    public bool debug;
+
+    bool _isVacuuming;
+    LayerMask _pigLayer;
+
+    void Awake()
+    {
+        _pigLayer = 1 << LayerMask.NameToLayer("Pig");
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            StartVacuum();
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            EndVacuum();
+        }
+
+        if (_isVacuuming)
+        {
+            Vacuum();
+        }
+    }
+
+    void StartVacuum()
+    {
+        _isVacuuming = true;
+        if (_vfx != null)
+        {
+            _vfx.Play();
+        }
+    }
+
+    void EndVacuum()
+    {
+        _isVacuuming = false;
+        if (_vfx != null)
+        {
+            _vfx.Stop();
+        }
+    }
+
+    void Vacuum()
+    {
+        ScanForPigs();
+    }
+
+    void ScanForPigs()
+    {
+        Collider[] pigs = Physics.OverlapSphere(transform.position, vacuumRadius, _pigLayer);
+
+        if (debug)
+        {
+            Debug.DrawRay(transform.position, transform.forward * vacuumRadius, Color.red);
+        }
+
+        foreach (Collider col in pigs)
+        {
+            if (PigInSuctionSector(col.transform))
+            {
+                //Suck pig here
+            }
+        }
+    }
+
+    bool PigInSuctionSector(Transform pig)
+    {
+        Vector3 playerPos = _vfx.transform.position;
+        playerPos.y = 0;
+        Vector3 pigPos = pig.position;
+        pigPos.y = 0;
+
+        Vector3 playerToPig = pigPos - playerPos;
+        float angle = Vector3.Angle(transform.forward, playerToPig);
+        return angle < vacuumingAngle; 
+    }
+}
